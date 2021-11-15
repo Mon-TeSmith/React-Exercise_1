98b9d075d35769eb25691a08ac18379c78613ecc
Te_Smith <93007222+Mon-TeSmith@users.noreply.github.com>
2021-11-15 14:30:38 -0600

    index.js file created

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import reactDom from 'react-dom';
+
+const jsxElement = <h1>Display Name Worksheet 1</h1>;
+
+reactDom.render(jsxElement, document.getElementById('root'));
