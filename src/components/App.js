be46f288855a58c721f5a4ab84975d79b651763b
Te_Smith <93007222+Mon-TeSmith@users.noreply.github.com>
2021-11-15 14:21:31 -0600

    React Components Worksheet 1

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import DisplayName from "./components/DisplayName/DisplayName"
+
+export default function App() {
+    return (
+        <div className="App">
+            <h1>React Components Worksheet 1</h1>
+            <h2>Import relevant component data</h2>
+            <DisplayName firstName="Aaron" lastName="Smith"/>
+        </div>
+    );
+} 
